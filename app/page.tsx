f6527fd90309de25fadb08a81cb07345c3e87083
jiangyi <jiangyi@corp-ci.com>
2023-03-31 19:01:41 +0800

    remove husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 
 export default function App() {
   return (
-    <>
       <Home />
-      <Analytics />
-    </>
   );
 }
